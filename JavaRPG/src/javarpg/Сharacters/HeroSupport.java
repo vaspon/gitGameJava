84d66ac4c81ea6628778531d62f8ac1a54bb515c
Doroga7 <71849387+Doroga7@users.noreply.github.com>
2020-09-29 18:18:37 +0300

    add

--- JavaRPG/src/javarpg/Сharacters/HeroSupport.java
+++ JavaRPG/src/javarpg/Сharacters/HeroSupport.java
@@ -1,4 +1,31 @@
 package javarpg.Сharacters;
 
-public class HeroSupport {
+public class HeroSupport extends GameObject {
+    public HeroSupport(String name,
+                       int life,
+                       boolean isAlive,
+                       int levelHero,
+                       int progress,
+                       String[] attribute,
+                       int health,
+                       int damage,
+                       int mana,
+                       String[] sinews,
+                       String[] skill,
+                       String[] shmot) {
+        super(
+                name = "Support",
+                life = 5,
+                isAlive = true,
+                levelHero = 1,
+                progress = 0,
+                attribute = new String[]{"1 отрибут", "2 отрибут", "3 отрибут"},
+                health = 100,
+                damage = 50,
+                mana = 10,
+                sinews = new String[]{"1 умение","2 умение", "3 умение"},
+                skill = new String[]{"1 персональное умение","2 персональное умение", "3 персональное умение"},
+                shmot= new String[]{"1 шмот","2 шмот", "3 шмот"}
+        );
+    }
 }
--- JavaRPG/src/javarpg/Сharacters/HeroTank.java
+++ JavaRPG/src/javarpg/Сharacters/HeroTank.java
@@ -14,18 +14,19 @@ public class HeroTank extends GameObject {
                     String[] skill,
                     String[] shmot) {
         super(
-                name,
-                life,
-                isAlive,
-                levelHero,
-                progress,
-                attribute,
-                health,
-                damage,
-                mana,
-                sinews,
-                skill,
-                shmot);
+                name = "Tank",
+                life = 2,
+                isAlive = true,
+                levelHero = 1,
+                progress = 0,
+                attribute = new String[]{"1 отрибут", "2 отрибут", "3 отрибут"},
+                health = 200,
+                damage = 50,
+                mana = 10,
+                sinews = new String[]{"1 умение","2 умение", "3 умение"},
+                skill = new String[]{"1 персональное умение","2 персональное умение", "3 персональное умение"},
+                shmot= new String[]{"1 шмот","2 шмот", "3 шмот"}
+                );
     }
 
 }
